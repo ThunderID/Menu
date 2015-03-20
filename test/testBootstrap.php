11efa219b054519f996e462ee5d29225536526ba
erickmo <erick.mo@vortege.com>
2015-03-20 18:50:59 +0700

    fixed testing bootstrap3menu

--- test/testBootstrap.php
+++ test/testBootstrap.php
@@ -1,12 +1,12 @@
 <?php 
 
-include '../src/Interface/IMenu.php';
-include '../src/Menu.php';
-include '../src/Bootstrap3Menu.php';
+include __DIR__ . '/../src/Interface/IMenu.php';
+include __DIR__ . '/../src/Menu.php';
+include __DIR__ . '/../src/Bootstrap3Menu.php';
 
 $menu = new \thunderid\menu\Bootstrap3Menu();
 $menu->add('dashboard', 'Dashboard', 'http://yahoo.com', null, '__left_nav');
-$menu->add('overview', 'Overview', 'http://yahoo.com', null, 'dashboard');
+$menu->add('overview', 'Overview', 'http://yahoo.com', 'glyphicon glyphicon-add', 'dashboard');
 
 echo $menu->render();
 
